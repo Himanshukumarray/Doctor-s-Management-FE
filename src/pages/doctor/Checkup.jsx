7920c4aba8d367f70ba2d67766b2f456b710b5a4
Himanshukumarray <himanshukumarray448@gmail.com>
2025-12-26 21:15:38 +0530

    update

--- src/pages/doctor/Checkup.jsx
+++ src/pages/doctor/Checkup.jsx
@@ -84,6 +84,7 @@ export default function Checkup() {
         onChange={(e) => setFormData({ ...formData, diagnosis: e.target.value })}
       />
 
+      {/* Symptoms */}
       <textarea
         placeholder="Symptoms"
         className="w-full p-3 border rounded mb-3"
