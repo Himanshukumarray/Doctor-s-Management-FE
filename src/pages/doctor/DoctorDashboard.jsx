366e89e663d2889b6841b59820d6bf1cc77eb94f
Himanshu Kumar Ray <126394498+Himanshukumarray@users.noreply.github.com>
2026-01-09 23:46:14 +0530

    update

--- src/pages/doctor/DoctorDashboard.jsx
+++ src/pages/doctor/DoctorDashboard.jsx
@@ -123,6 +123,7 @@ export default function DoctorDashboard() {
           </div>
         </div>
 
+        {/* Stats Cards */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mb-8">
           {stats.map((stat, index) => (
             <div
